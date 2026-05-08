cafba7694b3c8fc53a867866548d80cc0911f9dc
zefan <lordzefan@gmail.com>
2026-05-08 15:26:19 +0700

    add player health system

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : BaseHealth
+{
+    Animator animator;
+    protected override void Awake()
+    {
+        base.Awake();
+        animator = GetComponent<Animator>();
+    }
+    public override void OnTakeDamage(AttackSo attackSo)
+    {
+        base.OnTakeDamage(attackSo);
+        animator.Play("Hit", 1 ,0);
+    }
+}
